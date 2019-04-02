9a79921bad9d584212f8fe1f953667e1d063ea5f
Akshay Sharma <aksharma30498@gmail.com>
2019-04-02 23:03:31 +0530

    random

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true}).catch((e) => {
+  console.log(e);
+});
 
 module.exports ={mongoose};
