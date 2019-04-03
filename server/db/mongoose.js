274d917456c781ab7e6eaa28a87fee338cbb9f3b
Akshay Sharma <aksharma30498@gmail.com>
2019-04-03 14:28:40 +0530

    'fixes'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true}).catch((e) => {
-  console.log(e);
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true}).then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
 });
 
 module.exports ={mongoose};
