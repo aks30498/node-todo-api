155826f33023233f0bb0a53c3e8d1fd353f45938
Akshay Sharma <aksharma30498@gmail.com>
2019-04-04 12:29:37 +0530

    patch /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+const _ = require('lodash');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/Todo');
+const {User} = require('./models/User');
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -56,6 +57,32 @@ app.delete("/todos/:id", (req,res) => {
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch("/todos/:id", (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  var body = _.pick(req.body, ['text','completed']);
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
