4df8c8b239ba7064f3114c21caa20fdbca86624f
Akshay Sharma <aksharma30498@gmail.com>
2019-04-07 15:51:24 +0530

    auth, signup and GET /users/me

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true}).then(() => {
+mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true}).then(() => {
 console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env == "development"){
+  process.env.PORT = 3000;
+  process.env.MONGOLAB_URI = "mongodb://localhost:27017/TodoApp";
+}else if (env == "test") {
+  process.env.PORT = 3000;
+  process.env.MONGOLAB_URI = "mongodb://localhost:27017/TodoAppTest";
+  console.log("Test environment!");
+}
+
 const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
 const {User} = require('./models/User');
+const {authenticate} = require("./middleware/authenticate.js");
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,7 +23,7 @@ const {ObjectID} = require('mongodb');
 var app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.post("/todos", (req, res) => {
   var todo = new Todo({
@@ -92,8 +104,26 @@ app.patch("/todos/:id", (req,res) => {
   });
 });
 
+app.post("/users",(req,res)=>{
+  var body = _.pick(req.body, ['password','email']);
+  var user = new User(body);
+
+  user.save().then(()=>{
+     return user.generateAuthToken();
+  }).then((token)=>{
+     res.header('x-auth', token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  })
+});
+
+app.get("/users/me", authenticate, (req,res) => {
+  res.send(req.user);
+})
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
+
 
 module.exports ={app};
